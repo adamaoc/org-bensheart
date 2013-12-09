ea7db7cbba0038abd46c4c96b0e5128529505a49
adamaoc <adamaoc@gmail.com>
2013-12-08 21:42:51 -0600

    updated favicon and apple icon

--- views/layout/header.php
+++ views/layout/header.php
@@ -7,12 +7,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?php echo $Site->Headline; ?></title>
-        <meta name="description" content="">
+        <meta name="description" content="Ben's Heart was founded to help families going through the pains of having a child born with heart abnormalities.">
         <meta name="viewport" content="width=device-width">
+		<link rel="shortcut icon" href="favicon.ico">
+        <link rel="apple-touch-icon" href="assets/img/touch-icon-iphone.png" />
 
         <link rel="stylesheet" href="assets/css/main.css">
-
-        <script src="assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<script src="assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
         <a id="top"></a>
